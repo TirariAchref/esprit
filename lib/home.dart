7dd78e77705b0aa56e91c48f41ab52ecaab63659
TirariAchref <79058936+TirariAchref@users.noreply.github.com>
2021-11-18 15:03:21 +0100

    18/11/2021

--- lib/home.dart
+++ lib/home.dart
@@ -2,24 +2,110 @@ import 'package:flutter/material.dart';
 
 import 'product_info.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<GameData> games = [];
+  final description =  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+            "sed do eiusmod tempor incididunt ut "
+              "labore et dolore magna aliqua. Ut enim ad minim veniam, "
+             "quis nostrud exercitation ullamco laboris nisi ut "
+               "aliquip ex ea commodo consequat. Duis aute irure dolor "
+               "in reprehenderit in voluptate velit esse cillum dolore "
+               "eu fugiat nulla pariatur. Excepteur sint occaecat "
+           "cupidatat non proident, sunt in culpa qui officia "
+              "deserunt mollit anim id est laborum.";
+  @override
+  void initState() {
+    games.add(
+      GameData(
+        Image:"assets/images/dmc5.jpg" ,
+        Title: "Devil May Cry 5" ,
+        description: description ,
+        price: 200,
+        quantity:200
+         ),
+        );
+     games.add(
+      GameData(
+        Image:"assets/images/re8.jpg" ,
+        Title: "Resident Evil VIII" ,
+        description: description ,
+        price: 200,
+        quantity:200
+         ),
+        );
+         games.add(
+      GameData(
+        Image:"assets/images/nfs.jpg" ,
+        Title: "Need For Speed Heat" ,
+        description: description ,
+        price: 100,
+        quantity:200
+         ),
+        );
+         games.add(
+      GameData(
+        Image:"assets/images/rdr2.jpg" ,
+        Title:"Red Dead Redemption II" ,
+        description: description ,
+        price: 150,
+        quantity:200
+         ),
+        );
+           games.add(
+      GameData(
+        Image:"assets/images/fifa.jpg" ,
+        Title:"FIFA 22" ,
+        description: description ,
+        price: 100,
+        quantity:200
+         ),
+        );
+        
+        
+           
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("G-Store ESPRIT"),
       ),
-      body: ListView(
-        children: const [
-          ProductInfo("assets/images/dmc5.jpg", "Devil May Cry 5", 200),
-          ProductInfo("assets/images/re8.jpg", "Resident Evil VIII", 200),
-          ProductInfo("assets/images/nfs.jpg", "Need For Speed Heat", 100),
-          ProductInfo("assets/images/rdr2.jpg", "Red Dead Redemption II", 150),
-          ProductInfo("assets/images/fifa.jpg", "FIFA 22", 100)
-        ],
-      ),
+      body: ListView.builder(itemCount: games.length,itemBuilder:(context , index)
+      {
+        return ProductInfo(games[index].Image, games[index].Title, games[index].price);
+
+
+      })
     );
+  }
+}
+
+class GameData {
+  final String Image;
+  final String Title;
+  final int price;
+  final String description;
+  final int quantity;
+  GameData({
+    required this.Image,
+    required this.Title,
+    required this.price,
+    required this.description,
+    required this.quantity,
+  });
+
+  
+
+  @override
+  String toString() {
+    return 'GameData(Image: $Image, Title: $Title, price: $price, description: $description, quantity: $quantity)';
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,8 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'G-Store ESPRIT',
       // home: Signup(),
-      home: Signup(),
-      // ProductDetails("assets/images/dmc5.jpg", "Devil May Cry 5","fnmln", 200),
-      // home: ProductDetails("assets/images/dmc5.jpg", "Devil May Cry 5",
-      //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
-      //         "sed do eiusmod tempor incididunt ut "
-      //         "labore et dolore magna aliqua. Ut enim ad minim veniam, "
-      //         "quis nostrud exercitation ullamco laboris nisi ut "
-      //         "aliquip ex ea commodo consequat. Duis aute irure dolor "
-      //         "in reprehenderit in voluptate velit esse cillum dolore "
-      //         "eu fugiat nulla pariatur. Excepteur sint occaecat "
-      //         "cupidatat non proident, sunt in culpa qui officia "
-      //         "deserunt mollit anim id est laborum.", 200)
+      home: Home(),
+     
     );
   }
 }
