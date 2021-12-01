d7d5376dea43a165460d6a65b898b576bed58ad1
TirariAchref <79058936+TirariAchref@users.noreply.github.com>
2021-12-02 00:04:21 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
-<<<<<<< Updated upstream
-=======
+
 import 'package:esprit/authentifier.dart';
 import 'package:esprit/changemdp.dart';
 import 'package:esprit/panier.dart';
->>>>>>> Stashed changes
+
 import 'package:esprit/product_info.dart';
 import 'package:esprit/profile.dart';
 
 import 'package:flutter/material.dart';
 
-<<<<<<< Updated upstream
-=======
 import 'bibliotheque/bibliotheque.dart';
->>>>>>> Stashed changes
+
 import 'signup.dart';
 import 'product_details.dart';
 import 'home.dart';
@@ -29,18 +26,18 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
        debugShowCheckedModeBanner: false,
       title: 'G-Store ESPRIT',
-<<<<<<< Updated upstream
+
       // home: Signup(),
-      home: Home(),
+ 
      
-=======
+
      //  home:ProductDetails("assets/images/rdr2.jpg","Red Dead Redemption II", "fezf",200,10),
       //home: Home(),
      //home: Bibliotheque(),
   // home: Authentifier(),
    //home: Signup(),
-  home: Panier(),
->>>>>>> Stashed changes
+  home: changeMDP(),
+
     );
   }
 }
